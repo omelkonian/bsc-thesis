7e81091f240ccffa5d5cf73af7ddb69dbb1605bd
omelkonian <sdi1000128@di.uoa.gr>
2016-03-29 21:57:18 +0300

    Finished RelatedWork::BigData

--- code/akka.scala
+++ code/akka.scala
@@ -6,5 +6,5 @@ val g = FlowGraph { implicit b =>
   val merge = Merge[Int] 
   val f1, f2, f3, f4 = Flow[Int].map(_ + 10) 
   in ~> f1 ~> bcast ~> f2 ~> merge ~> f3 ~> out
-              bcast ~> f4 ~> merge
+                    bcast ~> f4 ~> merge
 }
