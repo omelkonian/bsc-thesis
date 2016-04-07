0182760a9ca163059299046542e202f2088d9336
omelkonian <sdi1000128@di.uoa.gr>
2016-04-07 19:02:09 +0300

    Finished Applications, Conclusions adn Abstract. Draft is ready.

---
+++ code/rxbeacon.java
@@ -0,0 +1,8 @@
+Stream.configure(new HazelcastDistributionStrategy(
+   	RxjavaEvaluationStrategy::new,
+   	MqttEvaluationStrategy::new
+));
+
+Stream.from(ReactiveBeacons.observe())
+   	.map(Beacon::getProximity)   
+   	.subscribe(new MqttTopic<>("/ble"));
